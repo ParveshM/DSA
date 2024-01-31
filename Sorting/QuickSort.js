937a002d433154858eba7fbb2cf4cc0c127ed9eb
Muhammed Parvesh <139102638+ParveshM@users.noreply.github.com>
2024-01-31 12:08:57 +0530

    updated Quick sort algorithm

--- Sorting/QuickSort.js
+++ Sorting/QuickSort.js
@@ -1,47 +1,33 @@
-function quickSort(arr) {
-    quickSortHelper(arr, 0, arr.length - 1);
-    return arr
-}
+function partition(arr, low, high) {
+  // last index aspivot element  
+  let pivot = arr[high];
+  let i = low ;
 
-function quickSortHelper(arr, startIndx, endIndx) {
-
-    if (startIndx >= endIndx) {
-        return;
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) { //if any element found less than pivot swap it 
+      swap(arr, i, j);
+      i++;
     }
-
-    let pivot = startIndx; // initailly taking the 0th element
-    let i = startIndx + 1; // i = large;
-    let j = endIndx; // j = small;
-
-    // Run the loop until the j or i cross each other;
-    while (i <= j) {
-
-        if (arr[i] > arr[pivot] && arr[j] < arr[pivot]) {
-            // if the i is greater than pivot and j is less than pivot ,then Swap elements;
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
-
-        if (arr[i] <= arr[pivot]) {
-            i++;
-        }
-
-        if (arr[j] >= arr[pivot]) {
-            j--;
-        }
-    }
-    // if the i or j crossed swap the element with pivot
-    swap(arr, j, pivot);
-    quickSortHelper(arr, startIndx, j - 1);
-    quickSortHelper(arr, j + 1, endIndx);
+  }
+  // move the pivot Element to its correct location and return the index 
+  swap(arr, i , high);
+  return i ;
 }
 
 function swap(arr, i, j) {
-    [arr[i], arr[j]] = [arr[j], arr[i]]
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
-
-
-const arr = [2, 30, 110, 41, 56, 12]
-
-console.log('Sorted :', quickSort(arr));
+function quickSort(arr, low, high) {
+  if (low < high) {
+    let p_location = partition(arr, low, high);
+    // left sub array sorting <<<<---
+    quickSort(arr, low, p_location - 1);
+    // Right sub array sorting ---->>>>
+    quickSort(arr, p_location + 1, high);
+  }
+  return arr;
+}
+const arr = [10, 80, 30, 90, 40];
+console.log(quickSort(arr, 0, arr.length - 1));
