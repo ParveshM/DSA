1c3dcd26f88dcf254472abcea845173336cfdc9f
Muhammed Parvesh <139102638+ParveshM@users.noreply.github.com>
2024-02-09 13:10:34 +0530

    Update recursion.js

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -8,12 +8,13 @@ const fact = 5
 const result = abc(fact)
 console.log(`Factorial of ${fact} is :`, result);
 
-// var fib = function (n) {
-//     if (n <= 1 ) { 
-//         return n
-//     } 
-//     return fib(n - 1) + fib(n - 2)
-// };
+// Fibbonocci
+var fib = function (n) {
+    if (n <= 1 ) { 
+        return n
+    } 
+    return fib(n - 1) + fib(n - 2)
+};
 
-// const result = fib()
-// console.log(result);
+const result = fib()
+console.log(result);
