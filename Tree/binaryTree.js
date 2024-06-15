cc25e7c0bb2975f361fea40dc39b6a74df625700
Muhammed Parvesh <139102638+ParveshM@users.noreply.github.com>
2024-06-15 17:55:53 +0530

    updated node deletion

--- Tree/binaryTree.js
+++ Tree/binaryTree.js
@@ -53,91 +53,59 @@ class BinarySearchTree {
         return false
     }
 
-    remove(data) {
-        this.removeHelper(data, this.root, null);
+
+    delete(value) {
+        this.root = this._deleteHelper(this.root, value);
     }
 
-    removeHelper(data, currentNode, parentNode) {
+    _deleteHelper(curr, value) {
+        if (!curr) return;
 
-        while (currentNode) {
-            if (data < currentNode.data) {
-                // Moves to the left subtree
-                parentNode = currentNode;
-                currentNode = currentNode.left;
-            } else if (data > currentNode.data) {
-                // Moves to the right subtree
-                parentNode = currentNode;
-                currentNode = currentNode.right;
-            } else {
-                // Node to be removed is found
-                if (currentNode.left !== null && currentNode.right !== null) {
-                    // Node has two children
-                    // Replace the node's data with the minimum value from the right subtree
-                    currentNode.data = this.getMinValue(currentNode.right)
-                    // Recursively remove the node with the minimum value from the right subtree
-                    this.removeHelper(data, currentNode.right, currentNode)
-                } else {
-                    // Node has at most one child
-                    if (parentNode === null) {
-                        // Node to be removed is the root
-                        if (currentNode.right === null) {
-                            this.root = currentNode.left
-                        } else {
-                            this.root = currentNode.right
-                        }
-                    } else {
-                        // Node to be removed is leaf node
-                        if (parentNode.left === currentNode) {
-                            // Node is a left child of its parent
-                            if (currentNode.right === null) {
-                                parentNode.left = currentNode.left
-                            } else {
-                                parentNode.left = currentNode.right
-                            }
-                        } else {
-                            // Node is a right child of its parent
-                            if (currentNode.right === null) {
-                                parentNode.right = currentNode.left
-                            } else {
-                                parentNode.right = currentNode.right
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-        }
-    }
-
-
-    getMinValue(currentNode) {
-        if (currentNode.left === null) {
-            return currentNode.data
+        if (value < curr.data) {
+            curr.left = this._deleteHelper(curr.left, value);
+        } else if (value > curr.data) {
+            curr.right = this._deleteHelper(curr.right, value);
         } else {
-            return this.getMinValue(currentNode.left)
+            if (!curr.left) return curr.right;
+            if (!curr.right) return curr.left;
+            let minNode = this.getMinNode(curr.right);
+            curr.data = minNode.data;
+
+            curr.right = this._deleteHelper(curr.right, minNode.data);
+        }
+        return curr;
+    }
+}
+
+
+getMinValue(currentNode) {
+    if (currentNode.left === null) {
+        return currentNode.data
+    } else {
+        return this.getMinValue(currentNode.left)
+    }
+}
+
+findClosest(target) {
+    let current = this.root;
+    let closest = current.data;
+
+    while (current) {
+        if (Math.abs(target - closest) > Math.abs(target - current.data)) {
+            closest = current.data;
+        }
+
+        if (target < current.data) {
+            current = current.left;
+        } else if (target > current.data) {
+            current = current.right;
+        } else {
+            break;
         }
     }
 
-    findClosest(target) {
-        let current = this.root;
-        let closest = current.data;
-
-        while (current) {
-            if (Math.abs(target - closest) > Math.abs(target - current.data)) {
-                closest = current.data;
-            }
-
-            if (target < current.data) {
-                current = current.left;
-            } else if (target > current.data) {
-                current = current.right;
-            } else {
-                break;
-            }
-        }
-
-        return closest;
-    }
+    return closest;
+}
 }
 
 const tree = new BinarySearchTree()
@@ -149,9 +117,6 @@ tree.insert(15)
 tree.insert(12)
 tree.insert(20)
 tree.insert(25)
-// tree.remove(25)
-console.log(tree.findClosest(2));
+// console.log(tree.findClosest(2));
 // console.log('Is value contains in the tree ?', tree.contains(10));
-// tree.remove(10)
-// console.log('Is value contains in the tree ?', tree.contains(10));
-console.log('\n', tree.root);
+tree.remove(10)
